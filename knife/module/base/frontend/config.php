dcc3aecec9f725ada178efb4cef96fe6b3b6c1e6
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2011-10-05 08:27:42 +0200

    Added the frontend config class

--- knife/module/base/frontend/config.php
+++ knife/module/base/frontend/config.php
@@ -1,1 +1,30 @@
 <?php
+
+/**
+ * This is the configuration-object
+ *
+ * @package		frontend
+ * @subpackage	subname
+ *
+ * @author		authorname
+ * @since		versionname
+ */
+final class FrontendclassnameConfig extends FrontendBaseConfig
+{
+	/**
+	 * The default action
+	 *
+	 * @var	string
+	 */
+	protected $defaultAction = 'index';
+
+
+	/**
+	 * The disabled actions
+	 *
+	 * @var	array
+	 */
+	protected $disabledActions = array();
+}
+
+?>
