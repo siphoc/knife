58a45f04141b6aa9a354689f4a1a4bb384f704dc
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2011-10-24 15:33:23 +0200

    Added the backend base ajax action

--- knife/ajax/base/backend/base.php
+++ knife/ajax/base/backend/base.php
@@ -1,1 +1,30 @@
 <?php
+
+/**
+ * This is an ajax handler
+ *
+ * @package		backend
+ * @subpackage	subname
+ *
+ * @author		authorname
+ * @since		versionname
+ */
+class BackendmodulenameAjaxactionname extends BackendBaseAJAXAction
+{
+	/**
+	 * Execute the action
+	 *
+	 * @return	void
+	 */
+	public function execute()
+	{
+		// call parent, this will probably add some general CSS/JS or other required files
+		parent::execute();
+
+		// get parameters
+		$itemId = (int) trim(SpoonFilter::getPostValue('id', null, '', 'int'));
+
+		// output
+		$this->output(self::OK, $return, FL::msg('Success'));
+	}
+}
